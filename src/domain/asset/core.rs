1da9b8692885abd1fceedd4ba83a10bb762bc72a
rezguiha <hamdi.rezgui1993@gmail.com>
2026-05-03 17:26:28 +0200

    core: make asset data public and add missing default factory for asset_id and connection_point_id.

--- src/domain/asset/core.rs
+++ src/domain/asset/core.rs
@@ -5,15 +5,19 @@ use garde::Validate;
 use serde::Deserialize;
 use typed_builder::TypedBuilder;
 use uuid::Uuid;
+// region: All asset types' physical input data.
+
 /// Bess system representing a
 /// unit on which we can collect scada data.
 #[derive(Debug, Validate, Deserialize, TypedBuilder)]
-struct BessData {
+pub struct BessData {
     /// The asset unique identification.
+    #[builder(default=Uuid::new_v4())]
     #[garde(skip)]
     asset_id: Uuid,
     /// The connection point identification which will serve
     /// as a grouping to model grid facing limitations/constraints.
+    #[builder(default=Uuid::new_v4())]
     #[garde(skip)]
     connection_point_id: Uuid,
     /// The declared future availability level of the Bess system
@@ -27,7 +31,7 @@ struct BessData {
 /// Combined Cycle Gas Turbine unit on which we can collect scada
 /// data.
 #[derive(Debug, Validate, Deserialize, TypedBuilder)]
-struct GasTurbineData {
+pub struct GasTurbineData {
     /// The asset unique identification.
     #[builder(default=Uuid::new_v4())]
     #[garde(skip)]
@@ -47,7 +51,7 @@ struct GasTurbineData {
 
 /// Renewable Asset unit on which we can collect scada data.
 #[derive(Debug, Validate, Deserialize, TypedBuilder)]
-struct RenewableData {
+pub struct RenewableData {
     /// The asset unique identification.
     #[builder(default=Uuid::new_v4())]
     #[garde(skip)]
