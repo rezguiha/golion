cd3b3ded96456abd9a1645e219882caa72a96550
rezguiha <hamdi.rezgui1993@gmail.com>
2026-05-16 17:29:16 +0200

    test: improve readibility of json printed in handler.

--- crates/golion-server/src/main.rs
+++ crates/golion-server/src/main.rs
@@ -1,10 +1,9 @@
 use axum::{Json, Router, routing::post};
 use golion_domain::asset::core::AssetData;
-
 /// Temporary handler that prints payload to make sure
 /// at least deserialization is working as expected.
 async fn handler_optimize(payload: Json<AssetData>) {
-    println!("{:?}", payload.0);
+    println!("{}", serde_json::to_string_pretty(&payload.0).unwrap());
 }
 
 #[tokio::main]
