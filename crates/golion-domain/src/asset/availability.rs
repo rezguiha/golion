f5d40be6e008658e48cfd9a6c8c2e90d4843e1d5
rezguiha <hamdi.rezgui1993@gmail.com>
2026-06-06 07:49:02 +0200

    core: get back to an array of struct usage instead of struct of array in timeseries data.

--- crates/golion-domain/src/asset/availability.rs
+++ crates/golion-domain/src/asset/availability.rs
@@ -4,47 +4,36 @@ use garde::Validate;
 use serde::{Deserialize, Serialize};
 use typed_builder::TypedBuilder;
 
-use crate::series::HasTimeStamps;
-
 /// Storage unit availability that can both charge and discharge.
 /// Represents declared planned availability for future dates/times.
 #[derive(Debug, Serialize, Deserialize, Validate, TypedBuilder)]
 pub struct StorageAvailability {
     /// Reference datetime for future declared availability.
     #[garde(skip)]
-    start_at: Vec<DateTime<Utc>>,
+    start_at: DateTime<Utc>,
     /// Maximum charge power with grid convention expressed in kW.
-    #[garde(inner(range(min = 0.0)))]
-    max_charge_power: Vec<f64>,
+    #[garde(range(min = 0.0))]
+    max_charge_power: f64,
     /// Maximum discharge power with grid convention expressed in kW.
-    #[garde(inner(range(min = 0.0)))]
-    max_discharge_power: Vec<f64>,
+    #[garde(range(min = 0.0))]
+    max_discharge_power: f64,
     /// Maximum usable energy expressed in kWh.
     /// Does not reflect energy level in battery — reflects maximum connected capacity.
-    #[garde(inner(range(min = 0.0)))]
-    max_usable_energy: Vec<f64>,
+    #[garde(range(min = 0.0))]
+    max_usable_energy: f64,
 }
-impl HasTimeStamps for StorageAvailability {
-    fn timestamps(&self) -> &[DateTime<Utc>] {
-        &self.start_at
-    }
-}
+
 /// Generator unit availability that can only output power.
 /// Represents declared planned availability for future dates/times.
 #[derive(Debug, Serialize, Deserialize, Validate, TypedBuilder)]
 pub struct GeneratorAvailability {
     /// Reference datetime for future declared availability.
     #[garde(skip)]
-    start_at: Vec<DateTime<Utc>>,
+    start_at: DateTime<Utc>,
     /// Maximum output power with grid convention expressed in kW.
-    #[garde(inner(range(min = 0.0)))]
-    max_output_power: Vec<f64>,
+    #[garde(range(min = 0.0))]
+    max_output_power: f64,
     /// Minimum output power with grid convention expressed in kW.
-    #[garde(inner(range(min = 0.0)))]
-    min_output_power: Vec<f64>,
-}
-impl HasTimeStamps for GeneratorAvailability {
-    fn timestamps(&self) -> &[DateTime<Utc>] {
-        &self.start_at
-    }
+    #[garde(range(min = 0.0))]
+    min_output_power: f64,
 }
