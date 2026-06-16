cb79fbd4ac69b2f9a3a02bacc71844bcd21b03e0
rezguiha <hamdi.rezgui1993@gmail.com>
2026-06-16 19:53:12 +0200

    core: add units module to common crate.

---
+++ crates/golion-common/src/units/power.rs
@@ -0,0 +1,54 @@
+use crate::serde_type;
+use chrono::Duration;
+use derive_more::{Add, From, Into};
+// region: Power Units
+serde_type! {
+#[derive(PartialEq, From, Add, Into, Debug, Clone, Copy)]
+pub struct KiloWatt(pub f64);}
+
+#[derive(PartialEq, From, Add, Into, Debug, Clone, Copy)]
+pub struct MegaWatt(pub f64);
+
+impl From<KiloWatt> for MegaWatt {
+    fn from(v: KiloWatt) -> Self {
+        let v_float: f64 = v.into();
+        MegaWatt(v_float / 1000.0)
+    }
+}
+// endregion: Power Units
+
+// region: Energy units
+#[derive(PartialEq, From, Add, Into, Debug, Clone, Copy)]
+pub struct KiloWattHour(pub f64);
+
+#[derive(PartialEq, From, Add, Into, Debug, Clone, Copy)]
+pub struct MegaWattHour(pub f64);
+
+impl From<KiloWattHour> for MegaWattHour {
+    fn from(v: KiloWattHour) -> MegaWattHour {
+        MegaWattHour(v.0 / 1000.0)
+    }
+}
+
+// endregion: Energy units
+
+// region: Power-Energy Conversion
+impl KiloWatt {
+    pub fn to_kwh(&self, duration: Duration) -> KiloWattHour {
+        KiloWattHour(self.0 * duration.as_seconds_f64() / 3600.0)
+    }
+    pub fn to_mwh(&self, duration: Duration) -> MegaWattHour {
+        self.to_kwh(duration).into()
+    }
+}
+impl MegaWatt {
+    pub fn to_kwh(&self, duration: Duration) -> KiloWattHour {
+        let value_kw: KiloWatt = KiloWatt(self.0 * 1000.0);
+        value_kw.to_kwh(duration)
+    }
+    pub fn to_mwh(&self, duration: Duration) -> MegaWattHour {
+        MegaWattHour(self.0 * duration.as_seconds_f64() / 3600.0)
+    }
+}
+
+// endregion: Power-Energy Conversion
