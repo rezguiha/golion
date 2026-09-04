1c14f5f13d5c01e2ccb72904342a254e7ebb276e
rezguiha <hamdi.rezgui.pro@gmail.com>
2026-09-04 15:29:56 +0200

    core: add possible products to market configuration.

--- crates/golion-domain/src/market/bid.rs
+++ crates/golion-domain/src/market/bid.rs
@@ -1,15 +1,22 @@
 /// Bid Minimal Defining Characteristics
 use crate::temporal::step::MinuteStep;
 use derive_more::{Add, From, Into};
+use jiff::Span;
 /// Represents market power increments.
 /// For example:
 ///     -ancillary markets : 1000 kW
 ///     -wholesale markets: 100 kW
-#[derive(PartialEq, From, Add, Into, Debug, Clone, Copy)]
+#[derive(PartialEq, From, Add, Into, Debug, Clone, Copy, Hash, Eq)]
 pub struct KiloWattIncrement(u16);
 
-#[derive(Debug)]
+#[derive(Debug, Hash, Eq, PartialEq)]
 pub struct BidSpecs {
     pub step: MinuteStep,
     pub increment: KiloWattIncrement,
+}
+
+impl BidSpecs {
+    pub fn try_new(step: Span, increment_kw: u16) -> crate::Result<Self> {
+        Ok(Self { step: step.try_into()?, increment: KiloWattIncrement(increment_kw) })
+    }
 }
